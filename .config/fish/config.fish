8b3c41ee5c21fad8ab510b62d2495eec66c6677e
stst5821 <stst5821@gmail.com>
2021-07-10 17:46:04 +0900

    tmux.conf修正

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,19 @@ set -x PATH $HOME/.anyenv/bin $PATH
 # fishにanyenvを入れる
 # シェルを立ち上げたときに読み込まれるファイルがある。.zrshとか
 # fishだとconfig.fishというのが読み込まれる。
+
 anyenv init - fish | source
-tmux
+# tmux
 
 # powerlinefontだと文字化けするので無効化する。
 set -g theme_powerline_fonts no
 
+# コマンド実行時間の非表示
+set -g theme_display_date no
+
 # vi mode
 fish_vi_key_bindings
-bind p fish_clipboard_paste 
+bind p fish_clipboard_paste
+
+set PATH /usr/local/bin /usr/sbin $PATH
+
